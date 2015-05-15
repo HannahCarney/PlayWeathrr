f2d9a46fe3ed8d3988cfb8a1798ac9445c436632
Hannah Carney <hannahcarneyart@gmail.com>
2015-05-15 03:32:25 +0100

    works now

--- PlayWeathr/ViewController.m
+++ PlayWeathr/ViewController.m
@@ -50,109 +50,102 @@ NSString *const FlickrAPIKey = @"9eb9449f0e7fd4350dc97be3d6a3b4fe";
     self.screenHeight = [UIScreen mainScreen].bounds.size.height;
     
     
-    UIImage *background = [UIImage imageNamed:@"bg"];
-    self.backgroundImageView = [[UIImageView alloc] initWithImage:background];
-    self.backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
-    [self.view addSubview: self.backgroundImageView];
-    self.blurredImageView = [[UIImageView alloc] init];
-    self.blurredImageView.contentMode = UIViewContentModeScaleAspectFill;
-    self.blurredImageView.alpha = 0;
-    [self.blurredImageView setImageToBlur:background blurRadius:10 completionBlock:nil];
-    [self.view addSubview:self.blurredImageView];
-    
-    
-    self.tableView = [[UITableView alloc] init];
-    self.tableView.backgroundColor = [UIColor clearColor];
-    self.tableView.delegate = self;
-    self.tableView.dataSource = self;
-    self.tableView.separatorColor = [UIColor colorWithWhite:1 alpha:0.2];
-    self.tableView.pagingEnabled = YES;
-    [self.view addSubview:self.tableView];
-    
-    CGRect headerFrame = [UIScreen mainScreen].bounds;
-    
-    CGFloat inset = 20;
-    
-    CGFloat temperatureHeight = 150;
-    CGFloat hiloHeight = 40;
-    CGFloat iconHeight = 30;
-    
-    CGRect temperatureFrame = CGRectMake(inset,
-                                         headerFrame.size.height - (temperatureHeight + hiloHeight),
-                                         headerFrame.size.width - (3 * inset),
-                                         temperatureHeight);
-    
-    CGRect iconFrame = CGRectMake(inset,
-                                  temperatureFrame.origin.y - iconHeight,
-                                  iconHeight,
-                                  iconHeight);
-    
-    CGRect conditionsFrame = iconFrame;
-    conditionsFrame.size.width = self.view.bounds.size.width - (((2 * inset) + iconHeight) + 10);
-    conditionsFrame.origin.x = iconFrame.origin.x + (iconHeight + 10);
-    
-    UIView *header = [[UIView alloc] initWithFrame:headerFrame];
-    header.backgroundColor = [UIColor clearColor];
-    self.tableView.tableHeaderView = header;
-    
-    // top
-    UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, self.view.bounds.size.width, 30)];
-    cityLabel.backgroundColor = [UIColor clearColor];
-    cityLabel.textColor = [UIColor whiteColor];
-    cityLabel.text = @"Loading...";
-    cityLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:18];
-    cityLabel.textAlignment = NSTextAlignmentCenter;
-    [header addSubview:cityLabel];
-    
-    
-    // bottom left
-    UILabel *temperatureLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
-    temperatureLabel.backgroundColor = [UIColor clearColor];
-    temperatureLabel.textColor = [UIColor blackColor];
-    temperatureLabel.text = @"0°";
-    temperatureLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:120];
-    [header addSubview:temperatureLabel];
-    
-    
-    // bottom right
-    UILabel *celciusLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
-    celciusLabel.backgroundColor = [UIColor clearColor];
-    celciusLabel.textColor = [UIColor blackColor];
-    celciusLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:75];
-    celciusLabel.textAlignment = NSTextAlignmentRight;
-    [header addSubview:celciusLabel];
-    
-    // bottom left
-    UILabel *conditionsLabel = [[UILabel alloc] initWithFrame:conditionsFrame];
-    conditionsLabel.backgroundColor = [UIColor clearColor];
-    conditionsLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:24];
-    conditionsLabel.textColor = [UIColor blackColor];
-    [header addSubview: conditionsLabel];
-    
-    // bottom left
-    UIImageView *iconView = [[UIImageView alloc] initWithFrame:iconFrame];
-    [iconView setTintColor:[UIColor blackColor]];
-    iconView.contentMode = UIViewContentModeScaleAspectFit;
-    iconView.backgroundColor = [UIColor clearColor];
-    [header addSubview:iconView];
+//    UIImage *background = [UIImage imageNamed:@"bg"];
+//    self.backgroundImageView = [[UIImageView alloc] initWithImage:background];
+//    self.backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
+//    [self.view addSubview: self.backgroundImageView];
+//    self.blurredImageView = [[UIImageView alloc] init];
+//    self.blurredImageView.contentMode = UIViewContentModeScaleAspectFill;
+//    self.blurredImageView.alpha = 0;
+//    [self.blurredImageView setImageToBlur:background blurRadius:10 completionBlock:nil];
+//    [self.view addSubview:self.blurredImageView];
+//    
+//    
+//    self.tableView = [[UITableView alloc] init];
+//    self.tableView.backgroundColor = [UIColor clearColor];
+//    self.tableView.delegate = self;
+//    self.tableView.dataSource = self;
+//    self.tableView.separatorColor = [UIColor colorWithWhite:1 alpha:0.2];
+//    self.tableView.pagingEnabled = YES;
+//    [self.view addSubview:self.tableView];
+//    
+//    CGRect headerFrame = [UIScreen mainScreen].bounds;
+//    
+//    CGFloat inset = 20;
+//    
+//    CGFloat temperatureHeight = 150;
+//    CGFloat hiloHeight = 40;
+//    CGFloat iconHeight = 30;
+//    
+//    CGRect temperatureFrame = CGRectMake(inset,
+//                                         headerFrame.size.height - (temperatureHeight + hiloHeight),
+//                                         headerFrame.size.width - (3 * inset),
+//                                         temperatureHeight);
+//    
+//    CGRect iconFrame = CGRectMake(inset,
+//                                  temperatureFrame.origin.y - iconHeight,
+//                                  iconHeight,
+//                                  iconHeight);
+//    
+//    CGRect conditionsFrame = iconFrame;
+//    conditionsFrame.size.width = self.view.bounds.size.width - (((2 * inset) + iconHeight) + 10);
+//    conditionsFrame.origin.x = iconFrame.origin.x + (iconHeight + 10);
+//    
+//    UIView *header = [[UIView alloc] initWithFrame:headerFrame];
+//    header.backgroundColor = [UIColor clearColor];
+//    self.tableView.tableHeaderView = header;
+//    
+//    // top
+//    UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, self.view.bounds.size.width, 30)];
+//    cityLabel.backgroundColor = [UIColor clearColor];
+//    cityLabel.textColor = [UIColor whiteColor];
+//    cityLabel.text = @"Loading...";
+//    cityLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:18];
+//    cityLabel.textAlignment = NSTextAlignmentCenter;
+//    [header addSubview:cityLabel];
+//    
+//    
+//    // bottom left
+//    UILabel *temperatureLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
+//    temperatureLabel.backgroundColor = [UIColor clearColor];
+//    temperatureLabel.textColor = [UIColor blackColor];
+//    temperatureLabel.text = @"0°";
+//    temperatureLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:120];
+//    [header addSubview:temperatureLabel];
+//    
+//    
+//    // bottom right
+//    UILabel *celciusLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
+//    celciusLabel.backgroundColor = [UIColor clearColor];
+//    celciusLabel.textColor = [UIColor blackColor];
+//    celciusLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:75];
+//    celciusLabel.textAlignment = NSTextAlignmentRight;
+//    [header addSubview:celciusLabel];
+//    
+//    // bottom left
+//    UILabel *conditionsLabel = [[UILabel alloc] initWithFrame:conditionsFrame];
+//    conditionsLabel.backgroundColor = [UIColor clearColor];
+//    conditionsLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:24];
+//    conditionsLabel.textColor = [UIColor blackColor];
+//    [header addSubview: conditionsLabel];
+//    
+//    // bottom left
+//    UIImageView *iconView = [[UIImageView alloc] initWithFrame:iconFrame];
+//    [iconView setTintColor:[UIColor blackColor]];
+//    iconView.contentMode = UIViewContentModeScaleAspectFit;
+//    iconView.backgroundColor = [UIColor clearColor];
+//    [header addSubview:iconView];
     
     
     [[RACObserve([ViewManager sharedManager], currentCondition)
       deliverOn:RACScheduler.mainThreadScheduler]
      subscribeNext:^(ViewCondition *newCondition) {
-         temperatureLabel.text = [NSString stringWithFormat:@"%.0f°",newCondition.temperature.floatValue];
-         celciusLabel.text = [NSString stringWithFormat:@"C"];
-         conditionsLabel.text = [newCondition.condition capitalizedString];
-         cityLabel.text = [newCondition.locationName capitalizedString];
-         iconView.image = [UIImage imageNamed:[newCondition imageName]];
-         if (cityLabel.text != nil)
-         {
-             [self searchFlickrPhotos: (@"%@", [cityLabel.text stringByReplacingOccurrencesOfString:@" " withString:@""])];
-            NSLog(@"Location name = %@", cityLabel.text);
+          [self searchFlickrPhotos: (@"%@", [newCondition.condition stringByReplacingOccurrencesOfString:@" " withString:@""])];
+            NSLog(@"Location name = %@", newCondition.condition);
            
          }
          
-     }];
+     ];
     
     
 
@@ -160,12 +153,11 @@ NSString *const FlickrAPIKey = @"9eb9449f0e7fd4350dc97be3d6a3b4fe";
     [[RACObserve([ViewManager sharedManager], dailyForecast)
       deliverOn:RACScheduler.mainThreadScheduler]
      subscribeNext:^(NSArray *newForecast) {
-         [self.tableView reloadData];
      }];
     
     
     [[ViewManager sharedManager] findCurrentLocation];
-}
+     }
 
 
 
@@ -353,6 +345,86 @@ NSString *const FlickrAPIKey = @"9eb9449f0e7fd4350dc97be3d6a3b4fe";
     self.tableView.separatorColor = [UIColor colorWithWhite:1 alpha:0.2];
     self.tableView.pagingEnabled = YES;
     [self.view addSubview:self.tableView];
+    
+    CGRect headerFrame = [UIScreen mainScreen].bounds;
+    
+    CGFloat inset = 20;
+    
+    CGFloat temperatureHeight = 150;
+    CGFloat hiloHeight = 40;
+    CGFloat iconHeight = 30;
+    
+    CGRect temperatureFrame = CGRectMake(inset,
+                                         headerFrame.size.height - (temperatureHeight + hiloHeight),
+                                         headerFrame.size.width - (3 * inset),
+                                         temperatureHeight);
+    
+    CGRect iconFrame = CGRectMake(inset,
+                                  temperatureFrame.origin.y - iconHeight,
+                                  iconHeight,
+                                  iconHeight);
+    
+    CGRect conditionsFrame = iconFrame;
+    conditionsFrame.size.width = self.view.bounds.size.width - (((2 * inset) + iconHeight) + 10);
+    conditionsFrame.origin.x = iconFrame.origin.x + (iconHeight + 10);
+    
+    UIView *header = [[UIView alloc] initWithFrame:headerFrame];
+    header.backgroundColor = [UIColor clearColor];
+    self.tableView.tableHeaderView = header;
+    
+    // top
+    UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, self.view.bounds.size.width, 30)];
+    cityLabel.backgroundColor = [UIColor clearColor];
+    cityLabel.textColor = [UIColor whiteColor];
+    cityLabel.text = @"Loading...";
+    cityLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:18];
+    cityLabel.textAlignment = NSTextAlignmentCenter;
+    [header addSubview:cityLabel];
+    
+    
+    // bottom left
+    UILabel *temperatureLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
+    temperatureLabel.backgroundColor = [UIColor clearColor];
+    temperatureLabel.textColor = [UIColor blackColor];
+    temperatureLabel.text = @"0°";
+    temperatureLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:120];
+    [header addSubview:temperatureLabel];
+    
+    
+    // bottom right
+    UILabel *celciusLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
+    celciusLabel.backgroundColor = [UIColor clearColor];
+    celciusLabel.textColor = [UIColor blackColor];
+    celciusLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:75];
+    celciusLabel.textAlignment = NSTextAlignmentRight;
+    [header addSubview:celciusLabel];
+    
+    // bottom left
+    UILabel *conditionsLabel = [[UILabel alloc] initWithFrame:conditionsFrame];
+    conditionsLabel.backgroundColor = [UIColor clearColor];
+    conditionsLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:24];
+    conditionsLabel.textColor = [UIColor blackColor];
+    [header addSubview: conditionsLabel];
+    
+    // bottom left
+    UIImageView *iconView = [[UIImageView alloc] initWithFrame:iconFrame];
+    [iconView setTintColor:[UIColor blackColor]];
+    iconView.contentMode = UIViewContentModeScaleAspectFit;
+    iconView.backgroundColor = [UIColor clearColor];
+    [header addSubview:iconView];
+
+    
+    [[RACObserve([ViewManager sharedManager], currentCondition)
+      deliverOn:RACScheduler.mainThreadScheduler]
+     subscribeNext:^(ViewCondition *newCondition) {
+         temperatureLabel.text = [NSString stringWithFormat:@"%.0f°",newCondition.temperature.floatValue];
+         celciusLabel.text = [NSString stringWithFormat:@"C"];
+         conditionsLabel.text = [newCondition.condition capitalizedString];
+         cityLabel.text = [newCondition.locationName capitalizedString];
+         iconView.image = [UIImage imageNamed:[newCondition imageName]];
+     }];
+
+        
 
 }
 
